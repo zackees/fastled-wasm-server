aa8aaf5f872e6c4156a3f4f48366c5e9852d90fa
zackees <z@zackees.com>
2025-05-12 14:46:35 -0700

    Remove unused shell scripts and initialization files

--- src/fastled_wasm_server/debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run this when you want to debug the compiler and you are in interactive
-# mode at /js
-
-python compile.py --debug --keep
--- src/fastled_wasm_server/paths.py
+++ src/fastled_wasm_server/paths.py
@@ -1,19 +1,17 @@
+import os
 from pathlib import Path
 
-UPLOAD_DIR = Path("/uploads")
-TEMP_DIR = Path("/tmp")
-OUTPUT_DIR = Path("/output")
+UPLOAD_DIR = Path(os.environ.get("FWS_UPLOAD_DIR", "/uploads"))
+TEMP_DIR = Path(os.environ.get("FWS_TEMP_DIR", "/tmp"))
+OUTPUT_DIR = Path(os.environ.get("FWS_OUTPUT_DIR", "/output"))
+COMPILER_ROOT = Path(os.environ.get("FWS_COMPILER_ROOT", "/js"))
+VOLUME_MAPPED_SRC = Path(os.environ.get("FWS_VOLUME_MAPPED_SRC", "/host/fastled/src"))
+LIVE_GIT_FASTLED_DIR = Path(os.environ.get("FWS_GIT_FASTLED_DIR", "/git/fastled"))
 
-COMPILER_ROOT = Path("/js")
+
 FASTLED_SRC = COMPILER_ROOT / "fastled" / "src"
-
-
 SKETCH_SRC = COMPILER_ROOT / "src"
-VOLUME_MAPPED_SRC = Path("/host/fastled/src")
 SKETCH_CACHE_FILE = OUTPUT_DIR / "compile_cache.db"
-LIVE_GIT_FASTLED_DIR = Path("/git/fastled")
-
-# COMPILER_DIR = Path("/js/compiler")
 COMPIER_DIR = COMPILER_ROOT / "compiler"
 FASTLED_COMPILER_DIR = COMPILER_ROOT / "fastled/src/platforms/wasm/compiler"
 PIO_BUILD_DIR = COMPILER_ROOT / ".pio/build"
