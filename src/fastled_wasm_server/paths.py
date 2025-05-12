99fc3a22ed16c8e38195a33e47bef01c54210a9e
zackees <z@zackees.com>
2025-05-12 14:52:25 -0700

    Update default paths in paths.py function and .gitignore

--- src/fastled_wasm_server/paths.py
+++ src/fastled_wasm_server/paths.py
@@ -2,17 +2,17 @@ import os
 from pathlib import Path
 
 
-def path_or_default(env_var: str, default: str) -> Path:
+def path_or_default(default: str, env_var: str) -> Path:
     """Return the path from the environment variable or the default."""
     return Path(os.environ.get(env_var, default))
 
 
-UPLOAD_DIR = path_or_default("ENV_UPLOAD_DIR", "/uploads")
-TEMP_DIR = path_or_default("ENV_TEMP_DIR", "/tmp")
-OUTPUT_DIR = path_or_default("ENV_OUTPUT_DIR", "/output")
-COMPILER_ROOT = path_or_default("ENV_COMPILER_ROOT", "/js")
-VOLUME_MAPPED_SRC = path_or_default("ENV_VOLUME_MAPPED_SRC", "/host/fastled/src")
-LIVE_GIT_FASTLED_DIR = path_or_default("ENV_GIT_FASTLED_DIR", "/git/fastled")
+UPLOAD_DIR = path_or_default("/uploads", "ENV_UPLOAD_DIR")
+TEMP_DIR = path_or_default("/tmp", "ENV_TEMP_DIR")
+OUTPUT_DIR = path_or_default("/output", "ENV_OUTPUT_DIR")
+COMPILER_ROOT = path_or_default("/js", "ENV_COMPILER_ROOT")
+VOLUME_MAPPED_SRC = path_or_default("/host/fastled/src", "ENV_VOLUME_MAPPED_SRC")
+LIVE_GIT_FASTLED_DIR = path_or_default("/git/fastled", "ENV_GIT_FASTLED_DIR")
 
 
 FASTLED_SRC = COMPILER_ROOT / "fastled" / "src"
