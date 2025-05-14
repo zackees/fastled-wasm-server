d760bf8db5263efd674b7357fd3481c9523e1a10
zackees <z@zackees.com>
2025-05-14 02:34:55 -0700

    Refactor source file resolver tests for clarity

--- tests/test_source_resolver.py
+++ tests/test_source_resolver.py
@@ -10,12 +10,21 @@ from fastled_wasm_server.server_serve_src_files import SourceFileFetcher
 _PREFIX_FASTLED = "/fastled/src"
 _PREFIX_SKETCH = "/js/src"
 
-_EXAMPLES_SRC_FASTLED: list[str] = [
-    "drawfsource/js/drawfsource/headers/FastLED.h",
-    "drawfsource/js/src/drawfsource/git/fastled/src/FastLED.h",
-]
 
-_EXAMPLES_SRC_SKETCH: list[str] = ["drawfsource/js/src/XYPath.ino.cpp"]
+_FASTLED_TEST_DATA: dict[str, str] = {
+    "drawfsource/js/drawfsource/headers/FastLED.h": f"{_PREFIX_FASTLED}/FastLED.h",
+    "drawfsource/js/src/drawfsource/git/fastled/src/FastLED.h": f"{_PREFIX_FASTLED}/FastLED.h",
+}
+
+_SKETCH_TEST_DATA: dict[str, str] = {
+    "drawfsource/js/src/XYPath.ino.cpp": f"{_PREFIX_SKETCH}/XYPath.ino.cpp",
+}
+
+
+_SOURCE_FILE_FETCHER: SourceFileFetcher = SourceFileFetcher(
+    fastled_src=Path(_PREFIX_FASTLED),
+    sketch_src=Path(_PREFIX_SKETCH),
+)
 
 
 class SourceFileResolver(unittest.TestCase):
@@ -23,34 +32,29 @@ class SourceFileResolver(unittest.TestCase):
 
     def test_fastled_patterns(self) -> None:
         """Test command line interface (CLI)."""
-        sfr: SourceFileFetcher = SourceFileFetcher(
-            fastled_src=Path(_PREFIX_FASTLED),
-            sketch_src=Path(_PREFIX_SKETCH),
-        )
 
-        for examples in _EXAMPLES_SRC_FASTLED:
-            resolved: Path | None = sfr.resolve_drawfsource(path=examples)
+        for url_browser, backend_path in _FASTLED_TEST_DATA.items():
+            resolved: Path | None = _SOURCE_FILE_FETCHER.resolve_drawfsource(
+                path=url_browser
+            )
             self.assertIsNotNone(resolved)
             self.assertEqual(
                 resolved,
-                Path(f"{_PREFIX_FASTLED}/FastLED.h"),
+                Path(backend_path),
             )
 
         print("done")
 
     def test_sketch_patterns(self) -> None:
         """Test command line interface (CLI)."""
-        sfr: SourceFileFetcher = SourceFileFetcher(
-            fastled_src=Path(_PREFIX_FASTLED),
-            sketch_src=Path(_PREFIX_SKETCH),
-        )
-
-        for examples in _EXAMPLES_SRC_SKETCH:
-            resolved: Path | None = sfr.resolve_drawfsource(path=examples)
+        for url_browser, backend_path in _SKETCH_TEST_DATA.items():
+            resolved: Path | None = _SOURCE_FILE_FETCHER.resolve_drawfsource(
+                path=url_browser
+            )
             self.assertIsNotNone(resolved)
             self.assertEqual(
                 resolved,
-                Path(f"{_PREFIX_SKETCH}/XYPath.ino.cpp"),
+                Path(backend_path),
             )
 
         print("done")
